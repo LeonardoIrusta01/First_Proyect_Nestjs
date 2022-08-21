97575f096c472bb23b213c08e632a09cac4fd290
Leonardo Gabriel Irusta <leoirusta502@gmail.com>
2022-08-21 19:37:06 -0300

    Aplicando Querys y organizando rutas dinamicas y no dinamicas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /* Rutas no Dinamicas */
   @Get()
   getHello(): string {
     return 'Hola mundo';
@@ -15,19 +15,45 @@ export class AppController {
     return 'Yo soy nuevo';
   }
 
-  /*  Forma mas sencilla de recibirlo
+  @Get('products/filter')
+  getProductFilter() {
+    return `yo soy un filter`;
+  }
+
+  /* Rutas Dinámicas */
+
+  /*  Forma mas sencilla de recibirlo */
   @Get('products/:productId')
   getProduct(@Param('productId') productId: string) {
     return `product ${productId}`;
   }
-  */
-  @Get('products/:id')
-  getProduct(@Param() params: any) {
-    return `product ${params.id}`;
+
+  /* Forma mas sencilla de manipular las Query */
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100, // No se hace falta especificar el valor del caractar al ya poner un valor especifico.
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products: limit => ${limit}, offset => ${offset}, brand => ${brand}`;
   }
 
   @Get('categories/:id/products/:productId')
   getCategory(@Param('productId') productId: string, @Param('id') id: string) {
     return `product ${productId} and ${id}`;
   }
+  /* Forma estandar de manejar las query
+    @Get('products')
+    getProducts(@Query() params: any) {
+      const { limit, offset} = params
+      return `products: limit => ${limit}, offset => ${offset}`;
+    }
+    */
+
+  /* Forma estandar de usar los Params
+    @Get('products/:id')
+    getProduct(@Param() params: any) {
+      return `product ${params.id}`;
+    }
+    */
 }
