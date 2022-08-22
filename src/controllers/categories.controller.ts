3b7596a7470b1d524bab97a8ddff7485f3c3e2a9
Leonardo Gabriel Irusta <leoirusta502@gmail.com>
2022-08-21 21:56:51 -0300

    Rutas de Post, Put y Delete agregadas

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
 
 @Controller('categories')
 export class CategoriesController {
@@ -7,10 +16,34 @@ export class CategoriesController {
   getHello(): string {
     return 'Hola Categories';
   }
+
+  @Post()
+  created(@Body() payload: any) {
+    return {
+      message: 'Categoria creada',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      message: `La categoría con el id ${id} fue actualizado`,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return {
+      message: `La categoría con el id ${id} fue eliminada`,
+    };
+  }
+
   /* Rutas Dinámicas */
 
   @Get(':id/products/:productId')
   getCategory(@Param('productId') productId: string, @Param('id') id: string) {
-    return `product ${productId} and ${id}`;
+    return { message: `product ${productId} and ${id}` };
   }
 }
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,20 +1,47 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   /* Rutas no Dinamicas */
-  
+
   @Get('filter')
   getProductFilter() {
-    return `yo soy un filter`;
+    return { message: `yo soy un filter` };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'accion de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      message: `El producto con el id ${id} fue actualizado`,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return {
+      message: `El producto con el id ${id} fue eliminada`,
+    };
   }
 
   /* Rutas Dinámicas */
-  /*  Forma mas sencilla de recibirlo */
-  @Get(':productId')
-  getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
-  }
 
   /* Forma mas sencilla de manipular las Query */
   @Get()
@@ -23,7 +50,14 @@ export class ProductsController {
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `products: limit => ${limit}, offset => ${offset}, brand => ${brand}`;
+    return {
+      message: `products: limit => ${limit}, offset => ${offset}, brand => ${brand}`,
+    };
   }
 
+  /*  Forma mas sencilla de recibirlo */
+  @Get(':productId')
+  getProduct(@Param('productId') productId: string) {
+    return { message: `product ${productId}` };
+  }
 }
